d6c9888ca6e939d29425209abc68f64f0d38bbd1
Arjav Jain <arjavjain0703@gmail.com>
2026-03-27 18:06:55 +0530

    hotfix for toolchain errors

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[unsafe(no_mangle)]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
 }
