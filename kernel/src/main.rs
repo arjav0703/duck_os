45631469c106e009acb3a755c61aa02e5aa482ea
Arjav Jain <arjavjain0703@gmail.com>
2026-03-28 11:54:18 +0530

    render text to vga buffer

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,7 +3,21 @@
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    static HELLO: &[u8] = b"Hello, world!";
+
+    let vga_buf = 0xb8000 as *mut u8;
+    render_string(HELLO, vga_buf);
+
     loop {}
+}
+
+fn render_string(s: &[u8], buf: *mut u8) {
+    for (i, &byte) in s.iter().enumerate() {
+        unsafe {
+            *buf.add(i * 2) = byte;
+            *buf.add(i * 2 + 1) = 0x0f; // White on black
+        }
+    }
 }
 
 use core::panic::PanicInfo;
