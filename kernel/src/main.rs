e2005d44af9e410b60318c7772b65e857ff889e9
Arjav Jain <arjavjain0703@gmail.com>
2026-03-31 20:20:52 +0530

    Ensure safety with lazystatic and mutex (spin)

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,12 +5,19 @@ mod vga;
 use vga::Writer;
 
 use core::fmt::Write;
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer::default());
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let mut writer = Writer::default();
-    write!(writer, "Welcome to DuckOS!\n").unwrap();
-    write!(writer, "<3").unwrap();
+    let mut w = WRITER.lock();
+
+    write!(w, "Welcome to DuckOS!\n");
+    write!(w, "<3");
 
     loop {}
 }
