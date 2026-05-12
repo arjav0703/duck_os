bf213aa5a7f4b3681e611da974980019007271b1
Arjav Jain <arjavjain0703@gmail.com>
2026-05-12 22:55:07 +0530

    feat: update executable

--- executable/src/main.rs
+++ executable/src/main.rs
@@ -1,14 +1,21 @@
 #![no_std]
 #![no_main]
 
+use kernel::println;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    println!("Hello from executable");
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
 
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(_: &PanicInfo) -> ! {
-    loop {}
-}
+// use core::panic::PanicInfo;
+//
+// #[panic_handler]
+// fn panic(_info: &PanicInfo) -> ! {
+//     loop {
+//         x86_64::instructions::hlt();
+//     }
+// }
